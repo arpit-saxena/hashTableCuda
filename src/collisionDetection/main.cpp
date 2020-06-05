22c0d8bc54710d49602acbf663a79555053eae38
Arpit Saxena <arpit.saxena2000@yahoo.in>
2020-06-05 09:51:58 +0530

    Update main to view bunny

--- src/collisionDetection/main.cpp
+++ src/collisionDetection/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <pcl/io/ply_io.h>
+#include <pcl/common/projection_matrix.h>
+#include <pcl/common/common.h>
+#include <pcl/point_cloud.h>
+//#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
-int main( int argc, char* argv[] )
+int main()
 {
-    return 0;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::io::loadPLYFile("bunny/reconstruction/bun_zipper.ply", *cloud);
+	pcl::visualization::PCLVisualizer viewer;
+	viewer.addPointCloud(cloud);
+	viewer.spin();
 }
