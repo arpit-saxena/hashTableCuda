7c5e9a768143a0d8ada1227a60578f8367ff1f3e
Anirudh Panigrahi <chiru9670@gmail.com>
2020-07-26 01:00:47 +0530

    Modify inserter, searcher and deleter to get src_key, src_value and src_lane by value Passing arguments by reference seems to slow down device functions in cuda, atleast on GTX 1050

--- src/SlabHash/HashTable.cuh
+++ src/SlabHash/HashTable.cuh
@@ -51,9 +51,9 @@ class HashTableOperation {		// a single object of this will reside on thread-loc
 	__device__ uint32_t ReadSlab(Address slab_addr, int laneID);
 	__device__ uint32_t * SlabAddress(Address slab_addr, int laneID);
 
-	__device__ void inserter(uint32_t s_read_data[], uint32_t &src_key, uint32_t &src_value, int &src_lane, uint32_t &work_queue, Address &next);
-	//__device__ void searcher(uint32_t s_read_data[], uint32_t &src_key, int &src_lane, uint32_t &work_queue, Address &next);
-	__device__ void deleter(uint32_t s_read_data[], uint32_t &src_key, uint32_t &src_value, int &src_lane, uint32_t &work_queue, Address &next);
+	__device__ void inserter(uint32_t s_read_data[], uint32_t src_key, uint32_t src_value, int src_lane, uint32_t &work_queue, Address &next);
+	//__device__ void searcher(uint32_t s_read_data[], uint32_t src_key, int src_lane, uint32_t &work_queue, Address &next);
+	__device__ void deleter(uint32_t s_read_data[], uint32_t src_key, uint32_t src_value, int src_lane, uint32_t &work_queue, Address &next);
 public:
 	__device__ HashTableOperation(const HashTable * const __restrict__ h, ResidentBlock * const __restrict__ rb);
 	__device__ void run(const Instruction::Type type, const uint32_t key, uint32_t value, bool is_active = true);
