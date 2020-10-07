7e51519ff272a7f7c4d60424f1ffa7236597f19c
Anirudh Panigrahi <chiru9670@gmail.com>
2020-10-07 18:14:13 +0530

    Implement markCollision for bounding_box approach This marks all collided triangles of mesh 1 by coloring them red Add a tester kernel to test collisionMarker::markCollision()

--- render/src/coll_det_funcs.cu
+++ render/src/coll_det_funcs.cu
@@ -1,6 +1,21 @@
 #include "render.cuh"
 
+__global__ void testmarking() {
+	int global_thread_id = blockDim.x * blockIdx.x + threadIdx.x;
+	while (global_thread_id < THREADS_PER_BLOCK * 64) {
+		collisionMarker::markCollision(0, global_thread_id);
+
+		global_thread_id += gridDim.x * blockDim.x;
+	}
+}
+
 __host__ bool CUDA::detectCollision(Mesh* d_meshes, HashTable* d_h) {
+	static int frame = 0;
+	if (frame++ > 120) {
+		int threadsPerBlock = THREADS_PER_BLOCK, numBlocks = 64;
+		testmarking<<<numBlocks, threadsPerBlock>>>();
+		return true;
+	}
 	bool collisionHappened = false;
 	return collisionHappened;
 }
@@ -19,5 +34,8 @@ __host__ void collisionMarker::init(Mesh meshes[2]) {
 
 __device__ void collisionMarker::markCollision(int voxel, int triangle_i)
 {
-	
+	Triangle* t = d_meshes[1] + triangle_i;
+	for (int i = 0; i < 3; ++i) {
+		t->vertices[i].hasCollided = 1.0f;
+	}
 }
